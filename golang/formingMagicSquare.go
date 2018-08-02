b1a63280a31ed307622b57a3de8e27b3cff176b5
Benjamin Prieur <benjamin@prieur.org>
2018-08-02 15:26:57 +0200

    Start FormingMagicSquare

---
+++ golang/formingMagicSquare.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func formingMagicSquare(s [][]int32) int32 {
+	return 0
+}
+
+func formingMagicSquareSample() {
+	magicSquare := [][]int32{{4, 8, 2}, {4, 5, 7}, {6, 1, 6}}
+	result := formingMagicSquare(magicSquare)
+	fmt.Println(result)
+}
